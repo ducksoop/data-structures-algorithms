13a02db3bfd4bd0b9e96197fb7302ba0c9958f8a
segfault0x61 <torresj1997@outlook.com>
2022-05-17 20:37:48 -0500

    feat: add singlylinkedlist implementation and tests

---
+++ Algorithms.Lib/SinglyLinkedList.cs
@@ -0,0 +1,86 @@
+﻿namespace Algorithms.Lib
+{
+    public class SinglyLinkedList<T>
+    {
+        public Node<T>? Head { get; private set; }
+        public Node<T>? Tail { get; private set; }
+
+        public int Count { get; private set; }
+
+        public void AddFirst(T value)
+        {
+            AddFirst(new Node<T>(value));
+        }
+
+        private void AddFirst(Node<T>? node)
+        {
+            // save off the current head
+            Node<T>? temp = Head;
+
+            Head = node;
+
+            // shifting the former head
+            Head.Next = temp;
+
+            Count++;
+
+            if (Count == 1)
+            {
+                Tail = Head;
+            }
+        }
+
+        public void AddLast(T value)
+        {
+            AddLast(new Node<T>(value));
+        }
+
+        private void AddLast(Node<T>? node)
+        {
+            if (IsEmpty)
+                Head = node;
+            else
+                Tail.Next = node;
+
+            Tail = node;
+
+            Count++;
+        }
+
+        public void RemoveFirst()
+        {
+            if (IsEmpty) throw new InvalidOperationException();
+
+            Head = Head?.Next;
+            if (Count == 1) Tail = null;
+
+            Count--;
+        }
+
+        public void RemoveLast()
+        {
+            if (IsEmpty) throw new InvalidOperationException();
+
+            if (Count == 1)
+            {
+                Head = Tail = null;
+            }
+            else
+            {
+                // find the penultimate node
+                var current = Head;
+                while (current?.Next != Tail)
+                {
+                    current = current?.Next;
+                }
+
+                current!.Next = null;
+                Tail = current;
+            }
+
+            Count--;
+        }
+
+        public bool IsEmpty => Count == 0;
+    }
+}
