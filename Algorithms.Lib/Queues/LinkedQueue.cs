74f229d6ca04298090b025e2e64acf7a3f37846f
segfault0x61 <torresj1997@outlook.com>
2022-05-18 23:19:49 -0500

    feat: add queue (linkedlist) implementation and tests

---
+++ Algorithms.Lib/Queues/LinkedQueue.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using Algorithms.Lib.Lists;
+
+namespace Algorithms.Lib.Queues
+{
+    public class LinkedQueue<T> : IEnumerable<T>
+    {
+        private readonly SinglyLinkedList<T> _list = new();
+
+        public int Count => _list.Count;
+        public bool IsEmpty => Count == 0;
+
+        public void Enqueue(T item)
+        {
+            _list.AddLast(item);
+        }
+
+        public void Dequeue()
+        {
+            _list.RemoveFirst();
+        }
+
+        public T Peek()
+        {
+            if (IsEmpty) throw new InvalidOperationException();
+
+            return _list.Head.Value;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _list.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
